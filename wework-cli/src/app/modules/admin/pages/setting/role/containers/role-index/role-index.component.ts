d094a2b2e44d57113ed37f9fd74c55a1dbac35e5
@smoreno <shamilmoreno@gmail.com>
2026-09-06 12:14:37 -0400

    Update

--- wework-cli/src/app/modules/admin/pages/setting/role/containers/role-index/role-index.component.ts
+++ wework-cli/src/app/modules/admin/pages/setting/role/containers/role-index/role-index.component.ts
@@ -1,37 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import Swal from 'sweetalert2';
+import { Component, OnInit } from "@angular/core";
+import Swal from "sweetalert2";
 
 // MODULES
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from "@angular/material/dialog";
 
 // MODELS
-import { ResponseModel } from '@core/models/response.model';
-import { RoleModel } from '@core/models/role.model';
-import { PermissionModel } from '@core/models/permission.model';
+import { ResponseModel } from "@core/models/response.model";
+import { RoleModel } from "@core/models/role.model";
+import { PermissionModel } from "@core/models/permission.model";
 
 // SERVICES
-import { RoleService } from '@core/services/role.service';
-import { PermissionService } from '@core/services/permission.service';
+import { RoleService } from "@core/services/role.service";
+import { PermissionService } from "@core/services/permission.service";
 
 // COMPONENTS
-import { RoleListComponent } from '../../components/role-list/role-list.component';
-import { RoleManageComponent } from '../../components/role-manage/role-manage.component';
-import { RolePermissionsComponent } from '../../components/role-permission/role-permission.component';
-import { RoleDeleteComponent } from '../../components/role-delete/role-delete.component';
+import { RoleListComponent } from "../../components/role-list/role-list.component";
+import { RoleManageComponent } from "../../components/role-manage/role-manage.component";
+import { RolePermissionsComponent } from "../../components/role-permission/role-permission.component";
+import { RoleDeleteComponent } from "../../components/role-delete/role-delete.component";
 
 @Component({
-	selector: 'app-role-index',
+	selector: "app-role-index",
 	standalone: true,
-	imports: [
-		MatDialogModule,
-		RoleListComponent,
-		RoleManageComponent,
-		RolePermissionsComponent,
-		RoleDeleteComponent
-	],
-	templateUrl: './role-index.component.html',
-	styleUrls: ['./role-index.component.scss'],
-	providers: [RoleService, PermissionService]
+	imports: [MatDialogModule, RoleListComponent, RoleManageComponent, RolePermissionsComponent, RoleDeleteComponent],
+	templateUrl: "./role-index.component.html",
+	styleUrls: ["./role-index.component.scss"],
+	providers: [RoleService, PermissionService],
 })
 export class RoleIndexComponent implements OnInit {
 	public roleList: RoleModel[] = [];
@@ -42,9 +36,9 @@ export class RoleIndexComponent implements OnInit {
 	public isTblLoading: boolean | undefined;
 	public breadscrums = [
 		{
-			title: 'Roles y Permisos',
-			items: ['Configuración'],
-			active: 'Roles y Permisos',
+			title: "Roles y Permisos",
+			items: ["Configuración"],
+			active: "Roles y Permisos",
 		},
 	];
 
@@ -52,7 +46,7 @@ export class RoleIndexComponent implements OnInit {
 		public dialogService: MatDialog,
 		private roleService: RoleService,
 		private permissionService: PermissionService,
-	) { }
+	) {}
 
 	ngOnInit(): void {
 		this.fetchRoleList();
@@ -61,9 +55,15 @@ export class RoleIndexComponent implements OnInit {
 
 	public actionCapture(info: any) {
 		switch (info.action.name) {
-			case 'manage': this.openManageDialog(info); break;
-			case 'permissions': this.openPermissionsDialog(info); break;
-			case 'delete': this.openDeleteDialog(info.data.row); break;
+			case "manage":
+				this.openManageDialog(info);
+				break;
+			case "permissions":
+				this.openPermissionsDialog(info);
+				break;
+			case "delete":
+				this.openDeleteDialog(info.data.row);
+				break;
 		}
 	}
 
@@ -73,11 +73,11 @@ export class RoleIndexComponent implements OnInit {
 			next: (rm: ResponseModel) => {
 				this.roleList = rm.response;
 				this.isTblLoading = false;
-				this.searchTittle = 'Filtrar Rol';
+				this.searchTittle = "Filtrar Rol";
 			},
-			error: err => {
+			error: (err) => {
 				const error: ResponseModel = err.error;
-				Swal.fire({ title: error.message, icon: 'info' });
+				Swal.fire({ title: error.message, icon: "info" });
 			},
 		});
 	}
@@ -87,9 +87,9 @@ export class RoleIndexComponent implements OnInit {
 			next: (rm: ResponseModel) => {
 				this.permissionList = rm.response;
 			},
-			error: err => {
+			error: (err) => {
 				const error: ResponseModel = err.error;
-				Swal.fire({ title: error.message, icon: 'info' });
+				Swal.fire({ title: error.message, icon: "info" });
 			},
 		});
 	}
@@ -97,7 +97,7 @@ export class RoleIndexComponent implements OnInit {
 	public openManageDialog(info: any) {
 		const role = info.data !== undefined ? info.data.row : undefined;
 		const roleManage = this.dialogService.open(RoleManageComponent, {
-			data: { role }
+			data: { role },
 		});
 
 		roleManage.componentInstance.saveRole.subscribe((data: any) => this.saveChanges(data));
@@ -107,43 +107,43 @@ export class RoleIndexComponent implements OnInit {
 		const role = info.data.row;
 		const rolePermissions = this.dialogService.open(RolePermissionsComponent, {
 			data: { role, permissionList: this.permissionList },
-			width: '600px'
+			width: "600px",
 		});
 
 		rolePermissions.componentInstance.savePermissions.subscribe((permissionIds: number[]) => {
 			this.roleService.setPermissions(role.id, permissionIds).subscribe({
 				next: (rm: ResponseModel) => {
 					rolePermissions.close();
-					Swal.fire({ title: rm.message, icon: 'success' });
+					Swal.fire({ title: rm.message, icon: "success" });
 				},
-				error: err => {
+				error: (err) => {
 					const error: ResponseModel = err.error;
-					Swal.fire({ title: error.message, icon: 'error' });
-				}
+					Swal.fire({ title: error.message, icon: "error" });
+				},
 			});
 		});
 	}
 
 	public openDeleteDialog(role: RoleModel) {
 		const roleDelete = this.dialogService.open(RoleDeleteComponent, {
-			data: { role }
+			data: { role },
 		});
 
 		roleDelete.componentInstance.removeRole.subscribe((roleId: number) => this.remove(roleId));
 	}
 
 	public saveChanges(values: any) {
-		const request = (values.op === 'update') ? 'update' : 'create';
+		const request = values.op === "update" ? "update" : "create";
 
 		this.roleService[request](values.object).subscribe({
 			next: (rm: ResponseModel) => {
 				this.fetchRoleList();
-				Swal.fire({ title: rm.message, icon: 'success' });
+				Swal.fire({ title: rm.message, icon: "success" });
 			},
-			error: err => {
+			error: (err) => {
 				const error: ResponseModel = err.error;
-				Swal.fire({ title: error.message, icon: 'error' });
-			}
+				Swal.fire({ title: error.message, icon: "error" });
+			},
 		});
 	}
 
@@ -152,10 +152,10 @@ export class RoleIndexComponent implements OnInit {
 			next: (rm: ResponseModel) => {
 				this.fetchRoleList();
 			},
-			error: err => {
+			error: (err) => {
 				const error: ResponseModel = err.error;
-				Swal.fire({ title: error.message, icon: 'error' });
-			}
+				Swal.fire({ title: error.message, icon: "error" });
+			},
 		});
 	}
 }
